faffa48bec10082d340fc176b721e5f9fb482de8
karim728 <adambo1@students.towson.edu>
2019-03-31 14:04:28 -0700

    created the substraction class in the main class to solve the error

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -5,6 +5,16 @@ public class SimpleCalculator {
 		return a + b;
 	}
 	
+	public int substraction(int a, int b) {
+		return a - b;
+	}
 	
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+	
+	public int division(int a, int b) {
+		return a / b;
+	}
 	
 }
